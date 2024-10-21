f046abc0214d4c5cfb8942b3c6a11893fd725e2b
alex_chan_7159 <115313791+alex-chan7159@users.noreply.github.com>
2024-10-21 19:39:10 -0400

    Optimizations
    Reduced number of api requests made

--- src/FreeRice.cpp
+++ src/FreeRice.cpp
@@ -63,19 +63,19 @@ size_t write_data(char *buffer, size_t itemsize, size_t nitems, void *ignorethis
     int linenumber = 1;
     //printf("New chunk (%zu bytes)\n", bytes);
     //printf("%d:\t", linenumber);
-    for (int i=0; i < bytes; i++) {
+    //for (int i=0; i < bytes; i++) {
         //printf("%c", buffer[i]);
-        if (buffer[i] == '\n') {
-            linenumber++;
+        //if (buffer[i] == '\n') {
+            //linenumber++;
             //printf("%d:\t", linenumber);
-        }
-    }
+        //}
+    //}
     
     json.append(buffer, bytes); // Append the buffer content to the global json string
    // for (int i=0; i < bytes; i++) {
         //cout << buffer[i];
     //}
-    printf("\n\n");
+    //printf("\n\n");
     return bytes;
 }
 
@@ -176,6 +176,10 @@ CURLcode submitAnswer(CURL *curl, const string &questionId, const string &select
         cerr << "Error: " << curl_easy_strerror(result) << endl; // Handle errors
     }
 
+    //cout << json;
+    cout << "We have made " << extractString(json, "\"rice\":", ",") << " rice with this game id!" << endl;
+    cout << "We have made " << extractString(json, "\"user_rice_total\":", ",") << " total rice!" << endl;
+
     // Cleanup
     curl_slist_free_all(headers); // Free the headers list
 
@@ -193,7 +197,7 @@ void extractGame() {
     gameId = extractString(json, "games\",\"id\":\"", "\"");
     //unordered_map<string, string> hash_map;
     questionWord = extractString(json, "\"text\":\"", " means:"); //word means!!! dont include the means
-    cout << "questionword: " << questionWord << "!!" << endl;
+    cout << "question word: " << questionWord << "!!" << endl;
     questionId = extractString(json, "\"question_id\":\"", "\"");
     //cout << "question word is " << questionWord << "and id is " << questionId << endl;
 
@@ -237,10 +241,10 @@ void extractGame() {
         answers.push_back(text);
         optionStart = optionEnd + 1; // Move past the current option
     }
-    for (const auto &pair : hash_map) {
-        cout << pair.first << ": " << pair.second << endl;
-    }
-    cout << "game id is: " << gameId << endl;
+    //for (const auto &pair : hash_map) {
+        //cout << pair.first << ": " << pair.second << endl;
+    //}
+    //cout << "game id is: " << gameId << endl;
 }
 
 map<string, float> getSimilarities(py::object nlp_model, string questionWord, vector<string> answers) {
@@ -278,6 +282,7 @@ int main(void) {
     // Create the NLP model
     py::object nlp_model = synonyms.attr("NLPModel")();
 
+    map<string, float> answerSimilarities;
     //map<string, float> answerSimilarities = getSimilarities(questionWord, answers);
 
     CURL *curl = curl_easy_init();
@@ -286,80 +291,78 @@ int main(void) {
         // Perform the request        
         CURLcode result = login(curl, readBuffer);
         
-if (token.empty()) {
-        token = extractString(json, "\"token\":\"", "\"");
-    }
-    
-    if (userId.empty()) {
-        userId = extractString(json, "\"uuid\":\"", "\"");
-    }
-
-        if (result != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << std::endl;
-        } else {
-            std::cout << "Response: " << readBuffer << std::endl;
-        }
-
-        cout << "userid is" << userId << "\n the token is " << token  << endl;
-        
-        do {
-            // Now you can make another API request using the token
-        result = apiRequestWithToken(curl, "https://engine.freerice.com/users/23add682-202f-4054-951c-7249e4e65917?_format=json", "get");
-        //cout << "blah blah blah" << json << endl;
-        string rice = extractString(json, "\"rice\":", ",");
-
-        cout << "We have made " << rice << " rice!!" << endl;
-        //const char* gameEndpoint = ("https://engine.freerice.com/games/" + gameId).c_str();
-        result = apiRequestWithToken(curl, "https://engine.freerice.com/games/cb2902c4-8085-43ad-a32c-139799b21ad2", "get");
-        //result = apiRequestWithToken(curl, gameEndpoint, "get");
-
-        //string questionId = "b755e843-8142-5cbd-81db-c8cfc3d3b51d"; // Example question ID
-        //string selectedOptionId = "609c5d21-29a0-4ef7-af90-b77e4857931a"; // Example selected option ID
-
-        //submitAnswer(curl, questionId, selectedOptionId); // Submit the answer
-        extractGame();
-
-        // Ensure that we have a valid question
-        if (questionWord == previousQuestion) {
-            cout << "Failed to retrieve new question." << endl;
-            break; // Exit if we can't get a new question
-        }
-        for (string answer : answers) {
-        cout << answer << endl;
-        }
-        map<string, float> answerSimilarities = getSimilarities(nlp_model, questionWord, answers);
-        string maxKey;
-        float maxValue = -1; // Initialize to the lowest possible float, wait no similarity sometimes returns negative
-
-        for (const auto& pair : answerSimilarities) {
-            if (pair.second > maxValue) {
-                maxValue = pair.second;
-                maxKey = pair.first;
+        if (token.empty()) {
+                token = extractString(json, "\"token\":\"", "\"");
             }
-        }
+            
+            if (userId.empty()) {
+                userId = extractString(json, "\"uuid\":\"", "\"");
+            }
 
-        //cout << maxKey << " has a sim of " << maxValue << endl;
-        //cout << questionId << endl;
-        //cout << questionId << "     " << hash_map[maxKey] << endl;
-        submitAnswer(curl, questionId, hash_map[maxKey]);
-        previousQuestion = questionWord;
+                if (result != CURLE_OK) {
+                    std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << std::endl;
+                } else {
+                    std::cout << "Response: " << readBuffer << std::endl;
+                }
 
-        this_thread::sleep_for(chrono::seconds(6));
-        cout << "AHHHHHHHHHHHHH" << endl;
-        //result = apiRequestWithToken(curl, "https://engine.freerice.com/games/cb2902c4-8085-43ad-a32c-139799b21ad2", "get");
-        //result = apiRequestWithToken(curl, gameEndpoint, "get");
-        
-        cout << "trying again" << endl;
-        json.clear();
-        } while (result == CURLE_OK);
+                cout << "userid is" << userId << "\n the token is " << token  << endl;
+                
+                result = apiRequestWithToken(curl, "https://engine.freerice.com/games/ce4d922a-29e3-4bcc-a10d-4ea1723155b8", "get"); //note!!! if you use a game id for too long, eventually the words become very difficult
+                do {
+                    // Now you can make another API request using the token
+                //result = apiRequestWithToken(curl, "https://engine.freerice.com/users/23add682-202f-4054-951c-7249e4e65917?_format=json", "get");
+                //cout << "blah blah blah" << json << endl;
+                //const char* gameEndpoint = ("https://engine.freerice.com/games/" + gameId).c_str();
+                
+                //result = apiRequestWithToken(curl, gameEndpoint, "get");
 
-        if (result != CURLE_OK) {
-            std::cerr << "API request failed: " << curl_easy_strerror(result) << std::endl;
-        }
+                //string questionId = "b755e843-8142-5cbd-81db-c8cfc3d3b51d"; // Example question ID
+                //string selectedOptionId = "609c5d21-29a0-4ef7-af90-b77e4857931a"; // Example selected option ID
 
-        // Clean up
-        //curl_slist_free_all(headers); // Free the header list
-        curl_easy_cleanup(curl); // Clean up curl session
+                //submitAnswer(curl, questionId, selectedOptionId); // Submit the answer
+                extractGame();
+                json.clear();
+                // Ensure that we have a valid question
+                if (questionWord == previousQuestion) {
+                    cout << "Failed to retrieve new question." << endl;
+                    break; // Exit if we can't get a new question
+                }
+                //for (string answer : answers) {
+                //cout << answer << endl;
+                //}
+                answerSimilarities = getSimilarities(nlp_model, questionWord, answers);
+                string maxKey;
+                float maxValue = -1; // Initialize to the lowest possible float, wait no similarity sometimes returns negative
+
+                for (const auto& pair : answerSimilarities) {
+                    if (pair.second > maxValue) {
+                        maxValue = pair.second;
+                        maxKey = pair.first;
+                    }
+                }
+
+                //cout << maxKey << " has a sim of " << maxValue << endl;
+                //cout << questionId << endl;
+                //cout << questionId << "     " << hash_map[maxKey] << endl;
+                submitAnswer(curl, questionId, hash_map[maxKey]);
+                previousQuestion = questionWord;
+
+                answerSimilarities.clear();
+                this_thread::sleep_for(chrono::seconds(5));
+                //cout << "AHHHHHHHHHHHHH" << endl;
+                //result = apiRequestWithToken(curl, "https://engine.freerice.com/games/cb2902c4-8085-43ad-a32c-139799b21ad2", "get");
+                //result = apiRequestWithToken(curl, gameEndpoint, "get");
+                
+                cout << "trying again" << endl;
+                } while (result == CURLE_OK);
+
+                if (result != CURLE_OK) {
+                    std::cerr << "API request failed: " << curl_easy_strerror(result) << std::endl;
+                }
+
+                // Clean up
+                //curl_slist_free_all(headers); // Free the header list
+                curl_easy_cleanup(curl); // Clean up curl session
 
     } else {
         std::cerr << "Curl initialization failed" << std::endl;
